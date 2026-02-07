d019c05e737f60759280716d5c61621ced07672e
Duo <xngc2021@gmail.com>
2026-02-08 03:16:03 +0800

    Initial commit

---
+++ src/pages/friends/kulipai.md
@@ -0,0 +1,7 @@
+---
+layout: ../../components/MarkdownPost.astro
+name: "苦小怕's Blog"
+description: "A student."
+avatar: "https://doc.kulipai.top/avator.png"
+url: "https://doc.kulipai.top"
+---
---
+++ src/sample.ts
@@ -0,0 +1,16 @@
+const author = {
+    name: `Duo`,
+    elaina: `Elaina`,
+    x: `https://x.com/Huluhuhululuhu`,
+    qq_group: `https://qm.qq.com/cgi-bin/qm/qr?k=atstG02pmxi1CRfzFC_zRgB0pVUOKSZf`,
+    bili: `https://space.bilibili.com/1850079922`,
+    github: `https://github.com/Duo-Star`,
+    email: "xngc2021@gmail.com",
+};
+
+const buy = {
+  title: `See on Github`,
+  link: `https://github.com/anthonylan/angie`,
+};
+
+export { author, buy };
